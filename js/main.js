e8e46a7015a5f1df612e9935e605fdb97e2b2ce0
Yogesh Kumar <yogeshkumar@197NODNB13718.(none)>
2014-11-11 12:46:44 +0530

    Started with the main email list and content.

--- js/main.js
+++ js/main.js
@@ -10,14 +10,34 @@ app.config(function($routeProvider) {
         controller: 'SettingsController'
     })
     .otherwise({
-        redirectTo: '/' 
+        redirectTo: '/'
     });
 });
 
-app.controller('HomeController', function(){
+app.controller('HomeController', function($scope){
 
 });
 
-app.controller('SettingsController', function(){
+app.controller('MailListingController', function($scope){
+  $scope.email = [{
+    'id': 1,
+    'from': 'yogi@fullstack.io',
+    'to' : 'ravi@fullstack.io',
+    'subject': 'Great job',
+    'body': 'Congrats on the release of the app'
+  }];
+});
 
+app.controller('ContentController', function($scope){
+
+});
+
+app.controller('SettingsController', function($scope){
+  $scope.settings = {
+    name: "Ari",
+    email: "me@example.com"
+  };
+  $scope.updateSettings = function() {
+    console.log("updateSettings was called.");
+  }
 });
